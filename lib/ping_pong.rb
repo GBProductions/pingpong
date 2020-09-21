0b112fefbe7c9ba36811ed6b48116b9c303e90d3
ajmarkow <alexanderjmarkow@gmail.com>
2020-09-21 13:57:20 -0700

    Update business logic.  Add garretts code to pass test.  Add failing test for match and replace function.

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,9 +1,20 @@
   def counts_up(usernumber)
     x = 0
     array = []
-    while (x < usernumber)
+    until x  >= usernumber
       x = x + 1
       array.push(x)
     end
-    return array
+  return array
+end
+
+  def match_and_replace_numbers(array)
+    replaced_array = []
+    array.each do |array_element|
+      element_as_integer = array.element.to_i
+      if  element_as_integer.include? "3"
+      replaced_array.push "Ping"
+      elsif element_as_integer.include? "5"
+      replaced_array.push "Pong"
+    end
   end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -6,3 +6,9 @@ describe ('counts_up') do
     expect(counts_up(2)).to(eq([1,2]))
   end
 end
+
+describe ('match_and_replace_numbers') do
+  it("replaces 3 with ping") do
+   expect(match_and_replace_numbers(3)).to(eq(["1","2","Ping"]))
+ end
+end
